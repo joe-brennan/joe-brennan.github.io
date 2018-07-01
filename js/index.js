06284066c7814cbaf85a98b5118156a417e09db9
J.Brennan <jb2050@kent.ac.uk>
2018-07-01 23:36:00 +0100

    changed colour of link

---
+++ js/index.js
@@ -0,0 +1,43 @@
+$(document).ready(() => {
+  $(".music").hover(() => {
+    $("body").css("background", "#DB3340");
+    $(".music").css("color", "#ffffff");
+    $("h1").css("color", "#ffffff");
+  }, () => {
+    $("body").css("background", "#ffffff");
+    $(".music").css("color", "#DB3340");
+    $("h1").css("color", "#000000");
+  });
+
+$(document).ready(() => {
+  $(".code").hover(() => {
+    $("body").css("background", "#DB3340");
+    $(".code").css("color", "#ffffff");
+    $("h1").css("color", "#ffffff");
+  }, () => {
+    $("body").css("background", "#ffffff");
+    $(".code").css("color", "#DB3340");
+    $("h1").css("color", "#000000");
+  });
+
+$(document).ready(() => {
+  $(".blog").hover(() => {
+    $("body").css("background", "#DB3340");
+    $(".blog").css("color", "#ffffff");
+    $("h1").css("color", "#ffffff");
+  }, () => {
+    $("body").css("background", "#ffffff");
+    $(".blog").css("color", "#DB3340");
+    $("h1").css("color", "#000000");
+  });
+
+$(document).ready(() => {
+  $(".instagram").hover(() => {
+    $("body").css("background", "#DB3340");
+    $(".instagram").css("color", "#ffffff");
+    $("h1").css("color", "#ffffff");
+  }, () => {
+    $("body").css("background", "#ffffff");
+    $(".instagram").css("color", "#DB3340");
+    $("h1").css("color", "#000000");
+  });
